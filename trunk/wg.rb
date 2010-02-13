e3b6d8730214e018a4d82f6a70218abff546d3a9
matteo.redaelli <matteo.redaelli@1d3b17e8-274b-11de-b16b-9b7f00df6c7c>
2010-02-13 14:44:31 +0000

    code cleanup

--- trunk/wg.rb
+++ trunk/wg.rb
@@ -1,15 +1,15 @@
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
 # any later version.
 #
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-#You should have received a copy of the GNU General Public License
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # see http://www.gnu.org/licenses/gpl-3.0.txt
 
 # == Synopsis
@@ -27,8 +27,8 @@
 # == Author ==
 #
 # Matteo Redaelli
-# web site: http://www.redaelli.org/matteo
-# blog: 
+# Web site: http://www.redaelli.org/matteo
+# Blog: http://www.redaelli.org/matteo/blog/
 
 require 'rubygems'
 require 'stomp'
@@ -179,7 +179,6 @@ class WG
       $stderr.flush
     end 
   end
-
 
   ####################################################################################
   # 
